657c0c958dba7a178dcaf836d0b1d0624ffe2ef9
vanisyarahma <vanisyarahma@outlook.com>
2025-12-26 11:36:32 +0700

    26/12/2025

---
+++ reserv.js
@@ -0,0 +1,57 @@
+console.log("JS jalan");
+
+// DATA
+let paket = null;
+let orang = null;
+let harga = null;
+let jam = null;
+
+// PILIH PAKET
+document.querySelectorAll(".paket-item").forEach(item => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".paket-item")
+      .forEach(p => p.classList.remove("active"));
+
+    item.classList.add("active");
+
+    paket = item.dataset.paket;
+    orang = item.dataset.orang;
+    harga = item.dataset.harga;
+
+    document.getElementById("jumlahOrang").innerText = orang + " orang";
+    document.getElementById("totalHarga").innerText =
+      "Rp " + Number(harga).toLocaleString("id-ID");
+  });
+});
+
+// PILIH JAM
+document.querySelectorAll(".jadwal").forEach(j => {
+  j.addEventListener("click", () => {
+    document.querySelectorAll(".jadwal")
+      .forEach(x => x.classList.remove("active"));
+
+    j.classList.add("active");
+    jam = j.innerText;
+  });
+});
+
+// TOMBOL BAYAR
+document.getElementById("btnBayar").addEventListener("click", () => {
+  const tanggal = document.getElementById("tanggal").value;
+
+  if (!paket || !tanggal || !jam) {
+    alert("Lengkapi semua pilihan dulu!");
+    return;
+  }
+
+  const data = {
+    paket,
+    orang,
+    harga,
+    tanggal,
+    jam
+  };
+
+  console.log("DATA RESERVASI:", data);
+  alert("Reservasi berhasil!\nCek console.");
+});
